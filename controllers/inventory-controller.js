4a8d3c2607aca2bb9553f37e0eeee4d58a7f9a95
kirandhatt <169182085+kirandhatt@users.noreply.github.com>
2024-06-25 13:29:22 -0700

    add validation and update inventory item in controllers

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -27,6 +27,17 @@ const checkWarehouseExists = async (req, res, next) => {
     next();
 };
 
+const checkInventoryExists = async (req, res, next) => {
+    const {id} = req.body;
+    const inventory = await knex('inventories').where({id}).first();
+
+    if (!inventory) {
+        return res.status(404).json({error: 'Inventory item not found.'});
+    }
+
+    next();
+}
+
 const createInventoryItem = async (req, res) => {
     try {
         const {warehouse_id, item_name, description, category, status, quantity} = req.body;
@@ -48,8 +59,34 @@ const createInventoryItem = async (req, res) => {
     }
 };
 
+const updateInventoryItem = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {warehouse_id, item_name, description, category, status, quantity} = req.body;
+
+        await knex('inventories')
+            .where({id})
+            .update({
+                warehouse_id,
+                item_name,
+                description,
+                category,
+                status,
+                quantity
+            });
+        
+        const updatedInventoryItem = await knex('inventories').where({id}).first();
+
+        res.status(200).json(updateInventoryItem);
+    } catch (error) {
+        res.status(500).json({error: 'Failed to update inventory item.'});
+    }
+};
+
 export {
     validateInventory,
     checkWarehouseExists,
+    checkInventoryExists,
     createInventoryItem,
+    updateInventoryItem
 };
--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -2,6 +2,8 @@ import express from "express";
 const router = express.Router();
 const inventoryController = require('../controllers/inventory-controller.js');
 
+// create new inventory item //
+
 router.post(
     '/',
     inventoryController.validateInventory,
