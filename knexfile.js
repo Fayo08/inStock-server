c00ff34be73538eb3df22d1d65121e81accc6536
kirandhatt <169182085+kirandhatt@users.noreply.github.com>
2024-06-25 10:16:54 -0700

    set up env variables, install knex

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+export default {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_LOCAL_HOST,
+    database: process.env.DB_LOCAL_DBNAME,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
+  }
+};
