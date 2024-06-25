5869ea85191b6bc3475c10fe7205cc0b70b90d0f
kirandhatt <169182085+kirandhatt@users.noreply.github.com>
2024-06-25 13:33:52 -0700

    define route for editing

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -11,4 +11,14 @@ router.post(
     inventoryController.createInventoryItem
 );
 
+// update existing inventory item //
+
+router.put(
+    '/:id',
+    inventoryController.validateInventory,
+    inventoryController.checkWarehouseExists,
+    inventoryController.checkInventoryExists,
+    inventoryController.updateInventoryItem
+);
+
 export default router;
