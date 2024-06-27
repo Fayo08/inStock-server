dbfc45203b743e836764aa4b9582d722cac756fc
alisonroot <alisonlroot@gmail.com>
2024-06-27 10:57:20 -0600

    added delete warehouse function and route

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -3,16 +3,17 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 const router = express.Router();
 import { validateWarehouse } from "../controllers/warehouse-controller.js";
 
-
 router.route("/").get(warehouseController.index);
-
 
 router.route("/").post(warehouseController.createNewWarehouse);
 
-router.route("/:id")
-  .get(warehouseController.findOne)
+router.route("/:id").get(warehouseController.findOne);
 
-router.route("/:id").put(validateWarehouse, warehouseController.updateWarehouse);
+router.route("/:id").delete(warehouseController.deleteWarehouse);
+
+router
+  .route("/:id")
+  .put(validateWarehouse, warehouseController.updateWarehouse);
 
 router.route("/:id/inventories").get(warehouseController.getInventory);
 
